870aa3ea3925d77c83de563fe46fddcf0e0df654
Allison Chen 2018 <achen8@students.d125.org>
2016-03-10 08:50:04 -0600

    wrote findZet, compiles, unsure if it works properly

---
+++ CodeHandouts/Group1/ZetCard.java
@@ -0,0 +1,56 @@
+import java.lang.Comparable;
+public class ZetCard extends Card
+{
+    
+    private int number; 
+    private int shape; 
+    private int fill; 
+    private int color;
+    
+    public ZetCard(int n, int s, int f, int c)
+    {
+        number = n; 
+        shape = s; 
+        fill = f; 
+        color = c;
+    }
+    public int getNumber()
+    {
+        return number;
+    }
+    public int getShape()
+    {
+        return shape;
+    }
+    public int getFill()
+    {
+        return fill;
+    }
+    public int getColor()
+    {
+        return color; 
+    }
+    public String toString()
+    {
+        String card = number + " ";
+        if (shape == 1)
+            card+="oval ";
+        if (shape == 2)
+            card +="squiggle ";
+        if (shape == 3)
+            card+="diamond ";
+        if (fill == 1)
+            card +="outlined ";
+        if (fill == 2)
+            card +="solid ";
+        if (fill == 3)
+            card +="striped ";
+        if (color == 1)
+            card +="red ";
+        if (color == 2)
+            card +="green ";
+        if (color == 3)
+            card +="blue "; 
+        return card;
+    }
+}
