fc6f850d4751aa5a78c7c90e49ddff0876df9b7d
anagpal8 <anagpal8@students.d125.org>
2016-03-14 08:32:16 -0500

    Added files via upload

---
+++ CodeHandouts/Group1/TestZetDeck.java
@@ -0,0 +1,11 @@
+public class TestZetDeck
+{
+
+    public TestZetDeck()
+    {
+        ZetDeck deck = new ZetDeck();
+        deck.takeTop();
+        deck.takeTop();
+        deck.takeTop();
+    }
+}
