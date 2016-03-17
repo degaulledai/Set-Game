428cc16432147c315b9850f3da51abee07bd3dff
Allison Chen 2018 <achen8@students.d125.org>
2016-03-17 10:41:12 -0500

    Uploading testZetAnalyzer file

---
+++ CodeHandouts/Group2/TestZetAnalyzer.java
@@ -0,0 +1,24 @@
+public class TestZetAnalyzer
+{
+    public static void main (String args [])
+    {
+        ZetDeck deck=new ZetDeck();
+        /*ZetTable table=new ZetTable();
+         * for (int i=0; i<table.length; i++)
+        {
+            for (int x=i+1; x<table.length; x++)
+            {
+                for (int y=x+1; y<table.length; y++)
+                {
+                    if (isZet(table[i],table[x],table[y])==true)
+                    {
+                        System.out.println("Set: "+table[i]+ ", "+table[x]+ ", "+table[y]);
+                    }
+                }
+            }
+        }
+         * System.out.println(ZetAnalyzer.findZet(table));
+         * 
+         */
+    }
+}
